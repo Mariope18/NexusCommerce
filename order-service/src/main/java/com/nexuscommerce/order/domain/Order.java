6a3e8beb66c60f7f65d54616bdfaf6d7870d4803
Mario Pella <pellamario18@gmail.com>
2026-07-04 11:16:46 +0200

    feature: completato modello di dominio per gli ordini

---
+++ order-service/src/main/java/com/nexuscommerce/order/domain/Order.java
@@ -0,0 +1,51 @@
+package com.nexuscommerce.order.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Order {
+
+    private UUID id;
+
+    private UUID customerId;
+
+    @Setter
+    private OrderStatus orderStatus;
+
+    private Set<OrderLine> orderLines = new HashSet<>();
+
+    private BigDecimal totalAmount = BigDecimal.ZERO;
+
+    @Builder
+    private Order(UUID id, UUID customerId, OrderStatus orderStatus) {
+        this.id = id;
+        this.customerId = customerId;
+        this.orderStatus = OrderStatus.PENDING;
+    }
+
+
+    public void aggiungiOrderLine(OrderLine orderLine) {
+        this.orderLines.add(orderLine);
+        this.totalAmount = calcoloTotalAmount();
+    }
+
+    public BigDecimal calcoloTotalAmount() {
+        return this.totalAmount = this.orderLines.stream()
+                .map(line -> {
+                    BigDecimal p = line.getPrice() != null ? line.getPrice() : BigDecimal.ZERO;
+                    long q = line.getQuantity() != null ? line.getQuantity() : 0L;
+                    return p.multiply(BigDecimal.valueOf(q));
+                })
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+}
---
+++ order-service/src/main/java/com/nexuscommerce/order/domain/OrderStatus.java
@@ -0,0 +1,7 @@
+package com.nexuscommerce.order.domain;
+
+public enum OrderStatus {
+    PENDING,
+    COMPLETED,
+    CANCELLED
+}
